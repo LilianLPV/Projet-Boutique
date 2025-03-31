dc7d09b94accc45cd0afb1ccb0f60b8502650d4e
Leroidesafk <leroidesafk.id@gmail.com>
2025-03-31 11:46:11 +0200

    schema finished

--- InWork/js/main.js
+++ InWork/js/main.js
@@ -27,3 +27,19 @@ document.querySelectorAll(".cath-button").forEach(button => {
     button.addEventListener("mouseenter", () => button.src = hoverSrc);
     button.addEventListener("mouseleave", () => button.src = originalSrc);
 });
+
+document.querySelectorAll(".swipper-arrow").forEach(arrow => {
+    const originalSrc = arrow.src;
+    const hoverSrc = originalSrc.replace(".png", "-hover.png");
+
+    // Appliquer l'effet de survol uniquement si l'image n'est pas 'none'
+    if (!originalSrc.includes("none")) {
+        arrow.addEventListener("mouseenter", () => {
+            arrow.src = hoverSrc;
+        });
+
+        arrow.addEventListener("mouseleave", () => {
+            arrow.src = originalSrc;
+        });
+    }
+});
